c0f4e003b8879723146c610f86a1b83383995315
ayannamodi <ayanna.modi@gmail.com>
2026-09-12 17:06:54 -0400

    task 2

--- exer4/warmUp.js
+++ exer4/warmUp.js
@@ -39,7 +39,7 @@ function toLetterGrade(numGrade) {
  * Example: [9, 3, 4, 1, 2, 0] --> [4, 2, 0]
  */
 function getEvenElements(array) {
-  // TODO
+  return array.filter((num) => num % 2 === 0);
 }
 
 /**
