a50c4347e49e477afb2e052adfb18bbf28033deb
ayannamodi <ayanna.modi@gmail.com>
2026-09-12 17:48:10 -0400

    task 5

--- exer4/warmUp.js
+++ exer4/warmUp.js
@@ -96,7 +96,11 @@ function combineObjects(object1, object2) {
  *  Example: [1, 2, 3] -> [3, 2, 1]
  */
 function reverseArr(array) {
-  // TODO
+  const reversed = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    reversed.push(array[i]);
+  }
+  return reversed;
 }
 
 // DO NOT EDIT BELOW THIS LINE -- the code is for testing purposes only!
